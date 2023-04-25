5c3af2d1628aada6b39cf48235ab4ab8f21db6f7
Caleb McKee <60406205+CMcKee714@users.noreply.github.com>
2023-04-24 23:35:59 -0700

    Completed exercises 1 - 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let newString = stringArray.reverse().join("");
 
+    // for (i = 0; i < stringArray.length + 1; i++) {
+    //     // let poppedCharacter = stringArray[i].pop();
+    //     // stringArray.unshift(poppedCharacter);
+    // }
+
+    // let newString = stringArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
