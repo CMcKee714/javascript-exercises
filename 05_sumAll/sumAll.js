fc62e1a9d287237aea796ead6c68547ca798b649
Caleb McKee <60406205+CMcKee714@users.noreply.github.com>
2023-04-30 23:02:59 -0700

    Completed challenges 4 and 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    let finalSum = 0;
+    let smallerNumber = Math.min(firstNumber, lastNumber);
+    let largerNumber = Math.max(firstNumber, lastNumber);
 
+    if (smallerNumber < 0 || 
+        isNaN(firstNumber) || 
+        isNaN(lastNumber) || 
+        typeof firstNumber === 'string' || 
+        typeof lastNumber === 'string') 
+        {   return 'ERROR'; }
+
+    else if (smallerNumber < largerNumber) {
+        let i = smallerNumber;
+
+        do {
+            finalSum += smallerNumber;
+            smallerNumber += 1;
+            i += 1;
+        } while (i <= largerNumber);
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
