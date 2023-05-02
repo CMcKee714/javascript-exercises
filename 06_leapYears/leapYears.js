5224884ca693dc6ac1fb1e3f3bac1992f6c4adb7
Caleb McKee <60406205+CMcKee714@users.noreply.github.com>
2023-05-01 20:08:30 -0700

    Solved challenges 06 and 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(value) {
+if (value % 100 == 0 && value % 400 !== 0) {
+    return false;
+} else if (value % 4 == 0) {
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let tempInC = (value - 32) * (5 / 9);
+  tempInC = +tempInC.toFixed(1);
+  return tempInC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let tempInF = value * (9 / 5) + 32;
+  tempInF = +tempInF.toFixed(1);
+  return tempInF;
 };
 
 // Do not edit below this line
