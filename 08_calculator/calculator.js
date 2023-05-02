b90005eb582af58a4fdb64b5ccf736ce304f94b1
Caleb McKee <60406205+CMcKee714@users.noreply.github.com>
2023-05-01 23:27:04 -0700

    Completed challenge 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(firstValue, secondValue) {
+	let finalValue = firstValue + secondValue;
+  return finalValue;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstValue, secondValue) {
+	lesserValue = Math.min(firstValue, secondValue);
+  greaterValue = Math.max(firstValue, secondValue);
+  finalValue = greaterValue - lesserValue;
+  return finalValue;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let finalSum = 0;
+  numbers.forEach(addNumbers);
+  
+  function addNumbers(numbers) {
+    finalSum += numbers;
+  }
+  return finalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let finalSum = numbers.reduce( (a, b) => a * b);
+  return finalSum;
 };
 
-const power = function() {
-	
+const power = function(firstValue, secondValue) {
+	let finalSum = Math.pow(firstValue, secondValue);
+  return finalSum;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	finalSum = 1;
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    for (let i = 2; i <= n; i++) {
+      finalSum *= i;
+    }
+  };
+  return finalSum;
 };
 
 // Do not edit below this line
