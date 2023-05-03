980a5426e6226db75fd947e005b86ada6a15ebbb
Caleb McKee <60406205+CMcKee714@users.noreply.github.com>
2023-05-02 22:57:02 -0700

    Completed challenged 09 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var re = /[\W_]/g;
+    let lowRegStr = str.toLowerCase().replace(re, "");
+    let reversedString = lowRegStr.split("").reverse().join("");
+    
+    return reversedString === lowRegStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    parseInt(n);
+    let n1 = 0, n2 = 1;
+    let nextNumber = 1;
+
+    if (n < 0) {
+        return "OOPS";
+    } else if (n <= 1) {
+        return nextNumber;
+    } else {
+        for (let i = 2; i <= n; i++) {
+            nextNumber = n1 + n2;
+            n1 = n2;
+            n2 = nextNumber;
+        }
+        return nextNumber;
+    }
 
 };
 
